a92b016761909d221e00452f65ab6678b938ff69
Quang Tran <nhatquang304@gmail.com>
2022-01-05 23:14:02 -0500

    fisrt app commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: ));
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

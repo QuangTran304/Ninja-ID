cc14ee7d791d00e5dafba762adc5d2efe09f406d
Quang Tran <nhatquang304@gmail.com>
2022-01-06 17:39:34 -0500

    finalize app with setState()

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ void main() {
 }
 
 class NinjaCard extends StatefulWidget {
-  const NinjaCard({ Key? key }) : super(key: key);
+  const NinjaCard({Key? key}) : super(key: key);
 
   @override
   State<NinjaCard> createState() => _NinjaCardState();
 }
 
 class _NinjaCardState extends State<NinjaCard> {
-
   int ninjaLevel = 0;
 
   @override
@@ -28,78 +27,88 @@ class _NinjaCardState extends State<NinjaCard> {
           backgroundColor: Colors.grey[850],
           elevation: 0,
         ),
-        body: Padding(
-          padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Center(
-                child: CircleAvatar(
-                  // Profile picture
-                  backgroundImage: Image.network('https://picsum.photos/120').image,
-                  radius: 60,
-                ),
-              ),
-              Divider(
-                height: 80,
-                color: Colors.grey[600],
-              ),
-              Text(
-                'NAME',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                'Quang Tran',
-                style: TextStyle(
-                  color: Colors.lightGreen,
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30,),
-              Text(
-                'NINJA LELVEL',
-                style: TextStyle(
-                  color: Colors.grey,
-                  letterSpacing: 2,
-                ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                '$ninjaLevel',
-                style: TextStyle(
-                  color: Colors.lightGreen,
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 30),
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.email,
-                    color: Colors.grey[400],
-                  ),
-                  SizedBox(width: 10),
-                  Text(
-                    'sample-email@test.com',
-                    style: TextStyle(
-                      color: Colors.grey[400],
-                      fontSize: 18,
-                      letterSpacing: 1,
-                    ),
-                  )
-                ],
-              )
-            ]
-          )
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              ninjaLevel += 1;
+            });
+          },
+          child: Icon(Icons.add),
+          backgroundColor: Colors.grey[800],
         ),
+        body: Padding(
+            padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Center(
+                    child: CircleAvatar(
+                      // Profile picture
+                      backgroundImage:
+                          Image.network('https://picsum.photos/120').image,
+                      radius: 60,
+                    ),
+                  ),
+                  Divider(
+                    height: 80,
+                    color: Colors.grey[600],
+                  ),
+                  Text(
+                    'NAME',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    'Quang Tran',
+                    style: TextStyle(
+                      color: Colors.lightGreen,
+                      letterSpacing: 2,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    'NINJA LELVEL',
+                    style: TextStyle(
+                      color: Colors.grey,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    '$ninjaLevel',
+                    style: TextStyle(
+                      color: Colors.lightGreen,
+                      letterSpacing: 2,
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.email,
+                        color: Colors.grey[400],
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'sample-email@test.com',
+                        style: TextStyle(
+                          color: Colors.grey[400],
+                          fontSize: 18,
+                          letterSpacing: 1,
+                        ),
+                      )
+                    ],
+                  )
+                ])),
       ),
     );
   }
